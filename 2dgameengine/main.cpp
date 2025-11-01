9bca5386b923933b3e42a1b86189e1576c80da1f
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-01 19:04:10 +0700

    feat: update libs

--- 2dgameengine/main.cpp
+++ 2dgameengine/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "SDL.h"
 
-int main() {
+int main(int argc, char* argv[]){
+    SDL_Init(SDL_INIT_EVERYTHING);
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
