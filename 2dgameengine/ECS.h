a79e2c54d5d456e2eebd50f25dff4049227aa915
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-04 11:48:04 +0700

    feat: update

--- 2dgameengine/ECS.h
+++ 2dgameengine/ECS.h
@@ -60,8 +60,49 @@ public:
 	template<typename TComponent> void RequireComponent();
 };
 
-class Registry {
+template <typename T>
+class Pool {
+private:
+	std::vector<T> m_data;
+public:
+	Pool(int size = 100) { Resize(size); }
+	~Pool() = default;
 
+	bool IsEmpty() const {
+		return m_data.empty();
+	}
+
+	int GetSize() const { return m_data.size(); }
+
+	void Resize(int n) {
+		m_data.resize(n);
+	}
+
+	void Clear() {
+		m_data.clear();
+	}
+
+	void Add(T Object) {
+		m_data.push_back(Object);
+	}
+
+	void Set(int index, T object) {
+		m_data[index] = object;
+	}
+
+	T& Get(int index) {
+		return static_cast<T&>(m_data[index]);
+	}
+
+	T& operator[] (unsigned int index) {
+		return m_data[index];
+	}
+};
+
+class Registry {
+private:
+	int m_numEntities = 0;
+	std::vector<Pool*> m_componentPools;
 };
 
 
