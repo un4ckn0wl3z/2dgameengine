ba215286a884c84d81de6c18ab235061457b605d
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-04 10:31:17 +0700

    feat: update

--- 2dgameengine/ECS.cpp
+++ 2dgameengine/ECS.cpp
@@ -1,24 +1,28 @@
 #include "ECS.h"
 
-int Entity::GetId() const
-{
+int Entity::GetId() const {
 	return m_id;
 }
 
-void System::AddEntityToSystem(Entity entity)
-{
+void System::AddEntityToSystem(Entity entity) {
+	m_entities.push_back(entity);
 }
 
-void System::RemoveEntityToSystem(Entity entity)
-{
+void System::RemoveEntityToSystem(Entity entity) {
+    m_entities.erase(
+        std::remove_if(
+            m_entities.begin(),
+            m_entities.end(),
+            [&entity](const Entity& other) { return entity.GetId() == other.GetId(); }
+        ),
+        m_entities.end()
+    );
 }
 
-std::vector<Entity> System::GetSystemEntity() const
-{
+std::vector<Entity> System::GetSystemEntity() const {
 	return m_entities;
 }
 
-const Signature& System::GetComponentSignature() const
-{
+const Signature& System::GetComponentSignature() const {
 	return m_componentSignature;
 }
