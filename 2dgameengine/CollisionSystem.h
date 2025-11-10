0004e451bc9dc3796cdf1349016654066d9e6a12
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-10 14:39:09 +0700

    feat: update

--- 2dgameengine/CollisionSystem.h
+++ 2dgameengine/CollisionSystem.h
@@ -15,13 +15,24 @@ public:
 	}
 
 	void Update() {
+		auto entities = GetSystemEntities();
+		for (auto i = entities.begin(); i != entities.end(); i++) {
+			Entity a = *i;
+			auto aTransfrom = a.GetComponent<TransformComponent>();
+			auto aCollider = a.GetComponent<BoxColliderComponent>();
+			// check right entities
+			for (auto j = i; j != entities.end(); j++) {
+				Entity b = *j;
+				if (a == b) continue; // bypass same entity
+				auto bTransfrom = b.GetComponent<TransformComponent>();
+				auto bCollider = b.GetComponent<BoxColliderComponent>();
+				// check collision
 
-		for (auto a : GetSystemEntities()) {
-			for (auto b : GetSystemEntities()) {
 
 
 
 			}
+
 		}
 		
 	}
