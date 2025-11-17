4e188d71e299974767a4073f4d249d5e44d9fae9
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-17 13:47:26 +0700

    feat: update

--- 2dgameengine/AssetStore.cpp
+++ 2dgameengine/AssetStore.cpp
@@ -2,6 +2,7 @@
 #include "Logger.h"
 #include "SDL_image.h"
 #include "SDL.h"
+#include "SDL_ttf.h"
 
 AssetStore::AssetStore() {
 	Logger::Log("AssetStore constructor called!");
@@ -17,10 +18,17 @@ void AssetStore::ClearAssets() {
 		SDL_DestroyTexture(texture.second);
 	}
 	m_textures.clear();
+
+	// clear fonts
+	for (auto font : m_fonts) {
+		TTF_CloseFont(font.second);
+	}
+	m_fonts.clear();
+
 	Logger::Log("AssetStore assets cleared!");
 }
 
-void AssetStore::AddAssets(SDL_Renderer* renderer, const std::string& assetId, const std::string filePath) {
+void AssetStore::AddTexture(SDL_Renderer* renderer, const std::string& assetId, const std::string filePath) {
 	SDL_Surface* surface = IMG_Load(filePath.c_str());
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 	SDL_FreeSurface(surface);
@@ -30,4 +38,14 @@ void AssetStore::AddAssets(SDL_Renderer* renderer, const std::string& assetId, c
 
 SDL_Texture* AssetStore::GetTexture(const std::string& assetId)  {
 	return m_textures[assetId];
+}
+
+void AssetStore::AddFont(const std::string& assetId, const std::string filePath, int fontSize) {
+
+	m_fonts.emplace(assetId, TTF_OpenFont(filePath.c_str(), fontSize));
+
+}
+
+TTF_Font* AssetStore::GetFont(const std::string& assetId) {
+	return m_fonts[assetId];
 }
--- 2dgameengine/AssetStore.h
+++ 2dgameengine/AssetStore.h
@@ -3,18 +3,28 @@
 #include <map>
 #include <string>
 #include "SDL.h"
+#include "SDL_ttf.h"
 
 class AssetStore {
 private:
 	std::map<std::string, SDL_Texture*> m_textures;
+	std::map<std::string, TTF_Font*> m_fonts;
 	// TODO: fonts, autio
 public:
 	AssetStore();
 	~AssetStore();
 
 	void ClearAssets();
-	void AddAssets(SDL_Renderer* renderer, const std::string& assetId, const std::string filePath);
+
+	void AddTexture(SDL_Renderer* renderer, const std::string& assetId, const std::string filePath);
 	SDL_Texture* GetTexture(const std::string& assetId) ;
+
+	void AddFont(
+		const std::string& assetId, 
+		const std::string filePath,
+		int fontSize
+		);
+	TTF_Font* GetFont(const std::string& assetId);
 
 
 
