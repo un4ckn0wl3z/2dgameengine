8f6e5bfb6fe1cccce2086d9c76f9ef4ecd65ff32
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-17 14:36:04 +0700

    feat: update

--- 2dgameengine/RenderTextSystem.h
+++ 2dgameengine/RenderTextSystem.h
@@ -1,8 +1,34 @@
 #pragma once
 #include "ECS.h"
+#include "TextLabelComponent.h"
+#include "SDL.h"
+#include "AssetStore.h"
+#include "SDL_ttf.h"
 
 class RenderTextSystem : public System {
 public:
-	RenderTextSystem() {}
+	RenderTextSystem() {
+		RequireComponent<TextLabelComponent>();
+	}
+
+	void Update(SDL_Renderer* renderer, SDL_Rect& camera, std::unique_ptr<AssetStore>& assetStore) {
+		for (auto entity : GetSystemEntities()) {
+			const auto textLabel = entity.GetComponent<TextLabelComponent>();
+			SDL_Surface* surface = 
+				TTF_RenderText_Blended(
+					assetStore->GetFont(textLabel.assetId),
+					textLabel.text.c_str(),
+					textLabel.color
+				);
+			SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+			SDL_FreeSurface(surface);
+			SDL_Rect dstRect = {
+				static_cast<int>(textLabel.postion.x - (textLabel.isFixed ? 0 : camera.x)),
+				static_cast<int>(textLabel.postion.y - (textLabel.isFixed ? 0 : camera.y)),
+			
+			};
+
+		}
+	}
 
 };
