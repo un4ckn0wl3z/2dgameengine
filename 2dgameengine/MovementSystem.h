88b6fb46af590886db957ac51a215ef79140cea8
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-20 10:03:22 +0700

    feat: update

--- 2dgameengine/MovementSystem.h
+++ 2dgameengine/MovementSystem.h
@@ -3,6 +3,7 @@
 #include "TransformComponent.h"
 #include "RigidBodyComponent.h"
 #include "Logger.h"
+#include "Game.h"
 
 class MovementSystem : public System {
 public:
@@ -21,8 +22,20 @@ public:
 
 			transform.position.x += rigidbody.velocity.x * deleta_time;
 			transform.position.y += rigidbody.velocity.y * deleta_time;
+			
+			bool isEntityOutsideMap = (
+				transform.position.x < 0 ||
+				transform.position.x > Game::s_mapWidth ||
+				transform.position.y < 0 ||
+				transform.position.y > Game::s_mapHeight
+				);
 
-			Logger::Log("Entity Id = " + std::to_string(entity.GetId()) + " position is now ( " + std::to_string(transform.position.x) + " ," + std::to_string(transform.position.y) + ")");
+			// remove entity that outside the map, except `player`
+			if (isEntityOutsideMap && !entity.Hastag("player")) {
+				entity.Kill();
+			}
+
+			// Logger::Log("Entity Id = " + std::to_string(entity.GetId()) + " position is now ( " + std::to_string(transform.position.x) + " ," + std::to_string(transform.position.y) + ")");
 
 		}
 	}
