b59007651a1fa1b2004c7acf0c55024750bf08c4
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-24 15:35:42 +0700

    feat: update

--- 2dgameengine/ScriptSystem.h
+++ 2dgameengine/ScriptSystem.h
@@ -3,27 +3,90 @@
 #include "ECS.h"
 #include "ScriptComponent.h"
 #include "TransformComponent.h"
+#include "RigidBodyComponent.h"
+#include "AnimationComponent.h"
+#include "ProjectileEmitterComponent.h"
 #include "Logger.h"
 #include "SDL.h"
 #include "lua.hpp"
 #include "sol.hpp"
 
-// native c++ for binding
-//GetEntityPosition(Entity entity) {
-//
-//}
+#include <tuple>
 
-void SetEntityPosition(Entity entity, double x, double y) {
-	if (entity.HasComponent<TransformComponent>()) {
-		auto& transform = entity.GetComponent<TransformComponent>();
-		transform.position.x = x;
-		transform.position.y = y;
-	}
-	else {
-		Logger::Err("Target entity has no transform component");
-	}
+std::tuple<double, double> GetEntityPosition(Entity entity) {
+    if (entity.HasComponent<TransformComponent>()) {
+        const auto transform = entity.GetComponent<TransformComponent>();
+        return std::make_tuple(transform.position.x, transform.position.y);
+    }
+    else {
+        Logger::Err("Trying to get the position of an entity that has no transform component");
+        return std::make_tuple(0.0, 0.0);
+    }
 }
 
+std::tuple<double, double> GetEntityVelocity(Entity entity) {
+    if (entity.HasComponent<RigidBodyComponent>()) {
+        const auto rigidbody = entity.GetComponent<RigidBodyComponent>();
+        return std::make_tuple(rigidbody.velocity.x, rigidbody.velocity.y);
+    }
+    else {
+        Logger::Err("Trying to get the velocity of an entity that has no rigidbody component");
+        return std::make_tuple(0.0, 0.0);
+    }
+}
+
+void SetEntityPosition(Entity entity, double x, double y) {
+    if (entity.HasComponent<TransformComponent>()) {
+        auto& transform = entity.GetComponent<TransformComponent>();
+        transform.position.x = x;
+        transform.position.y = y;
+    }
+    else {
+        Logger::Err("Trying to set the position of an entity that has no transform component");
+    }
+}
+
+void SetEntityVelocity(Entity entity, double x, double y) {
+    if (entity.HasComponent<RigidBodyComponent>()) {
+        auto& rigidbody = entity.GetComponent<RigidBodyComponent>();
+        rigidbody.velocity.x = x;
+        rigidbody.velocity.y = y;
+    }
+    else {
+        Logger::Err("Trying to set the velocity of an entity that has no rigidbody component");
+    }
+}
+
+void SetEntityRotation(Entity entity, double angle) {
+    if (entity.HasComponent<TransformComponent>()) {
+        auto& transform = entity.GetComponent<TransformComponent>();
+        transform.rotation = angle;
+    }
+    else {
+        Logger::Err("Trying to set the rotation of an entity that has no transform component");
+    }
+}
+
+void SetEntityAnimationFrame(Entity entity, int frame) {
+    if (entity.HasComponent<AnimationComponent>()) {
+        auto& animation = entity.GetComponent<AnimationComponent>();
+        animation.currentFrame = frame;
+    }
+    else {
+        Logger::Err("Trying to set the animation frame of an entity that has no animation component");
+    }
+}
+
+void SetProjectileVelocity(Entity entity, double x, double y) {
+    if (entity.HasComponent<ProjectileEmitterComponent>()) {
+        auto& projectileEmitter = entity.GetComponent<ProjectileEmitterComponent>();
+        projectileEmitter.projectileVelocity.x = x;
+        projectileEmitter.projectileVelocity.y = y;
+    }
+    else {
+        Logger::Err("Trying to set the projectile velocity of an entity that has no projectile emitter component");
+    }
+}
 
 class ScriptSystem : public System {
 public:
@@ -32,19 +95,25 @@ public:
 		RequireComponent<ScriptComponent>();
 	}
 
-	void CreateLuaBindings(sol::state& lua) {
-		// create "entity" user-type
-		lua.new_usertype<Entity>(
-			"entity",
-			"get_id", &Entity::GetId,
-			"destroy", &Entity::Kill,
-			"has_tag", &Entity::Hastag,
-			"belongs_to_group", &Entity::BelongsToGroup
-		);
+    void CreateLuaBindings(sol::state& lua) {
+        // Create the "entity" usertype so Lua knows what an entity is
+        lua.new_usertype<Entity>(
+            "entity",
+            "get_id", &Entity::GetId,
+            "destroy", &Entity::Kill,
+            "has_tag", &Entity::Hastag,
+            "belongs_to_group", &Entity::BelongsToGroup
+        );
 
-		lua.set_function("set_position", SetEntityPosition);
-	}
-
+        // Create all the bindings between C++ and Lua functions
+        lua.set_function("get_position", GetEntityPosition);
+        lua.set_function("get_velocity", GetEntityVelocity);
+        lua.set_function("set_position", SetEntityPosition);
+        lua.set_function("set_velocity", SetEntityVelocity);
+        lua.set_function("set_rotation", SetEntityRotation);
+        lua.set_function("set_projectile_velocity", SetProjectileVelocity);
+        lua.set_function("set_animation_frame", SetEntityAnimationFrame);
+    }
 	void Update(double deltaTime, int ellapsedTime) {
 		for (auto entity: GetSystemEntities()) {
 			const auto script = entity.GetComponent<ScriptComponent>();
--- 2dgameengine/assets/scripts/Level1.lua
+++ 2dgameengine/assets/scripts/Level1.lua
@@ -218,7 +218,7 @@ Level = {
                     function(entity, delta_time, ellapsed_time)
                         -- print("Executing the SU-27 fighter jet Lua script!")
 
-                        --[[
+                        
                         -- this function makes the fighter jet move up and down the map shooting projectiles
                         local current_position_x, current_position_y = get_position(entity)
                         local current_velocity_x, current_velocity_y = get_velocity(entity)
@@ -238,7 +238,7 @@ Level = {
                             set_rotation(entity, 180) -- point down
                             set_projectile_velocity(entity, 0, 200) -- shoot projectiles down
                         end
-                        --]]
+                        
                     end
                 }
             }
