f632ed165aabd76fff8d13811aa248c70e895214
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-24 14:46:23 +0700

    feat: update

--- 2dgameengine/assets/scripts/Level1.lua
+++ 2dgameengine/assets/scripts/Level1.lua
@@ -165,14 +165,14 @@ Level = {
                 on_update_script = {
                     [0] =
                     function(entity, delta_time, ellapsed_time)
-                        print("Executing F-22 Lua script!")
+                        -- print("Executing F-22 Lua script!")
 
-                        --[[
+                        
                         -- change the position of the the airplane to follow a sine wave movement
                         local new_x = ellapsed_time * 0.09
                         local new_y = 200 + (math.sin(ellapsed_time * 0.001) * 50)
                         set_position(entity, new_x, new_y) -- set the new position
-                        --]]
+                        
                     end
                 }
             }
